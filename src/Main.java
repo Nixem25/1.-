8881e30a08f7b6b340620ea59c64273af6c47a2f
Nixem25 <nixem256@gmail.com>
2021-05-19 16:42:31 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      int  price = 16550;
+      int rubPerMile = 20;
+      int miles = price / rubPerMile;
+      System.out.println("You will receive "+miles+" bonus miles for this ticket");
+    }
+}
